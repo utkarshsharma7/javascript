aef6516054c9b3c03ba304a6fd14277fd76ae423
Utkarsh Sharma <95753872+utkarshsharma7@users.noreply.github.com>
2023-12-16 18:07:24 +0000

    16dec

---
+++ 01basics/06_nums_and_maths.js
@@ -0,0 +1,23 @@
+// const score =400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+// ++++++++++++++MATHS++++++++++++++++
+
+console.log(Math);
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+ min);
+ 
